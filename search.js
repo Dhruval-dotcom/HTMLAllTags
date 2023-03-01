45b8c810b936c7fc9cd825255aadcd0b784b4103
dhruval <dhruvaldevaliya@gmail.com>
2023-03-01 14:18:03 +0530

    Added all files

---
+++ search.js
@@ -0,0 +1,6 @@
+function pageRedirect() {
+    alert(document.getElementById("browser").value)
+    window.location.replace("https://www.tutorialrepublic.com/?"+document.getElementById("browser").value);
+}      
+                
+          
